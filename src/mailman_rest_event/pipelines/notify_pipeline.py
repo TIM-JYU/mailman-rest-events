62d5f94991014ab3f90b7c7c3db7ca4fc909cb32
dezhidki <dezhidki@jyu.fi>
2021-04-10 21:44:11 +0300

    Fix new handlers not being added correctly

--- src/mailman_rest_event/pipelines/notify_pipeline.py
+++ src/mailman_rest_event/pipelines/notify_pipeline.py
@@ -18,9 +18,10 @@ class NotifyPipeline:
         for name, pipeline in config.pipelines.items():
             logger.info(f"Pipeline: {name}")
             try:
-                if "to-archive" in pipeline._handlers and "to-notify" not in pipeline._handlers:
+                handler_names = [ h.name for h in pipeline._handlers ]
+                if "to-archive" in handler_names and "to-notify" not in handler_names:
                     logger.info(f"Added to-notify to {name}")
-                    pipeline._handlers.append("to-notify")
+                    pipeline._handlers.append(config.handlers["to-notify"])
             except Exception as e:
                 logger.error(f"Error: {e}")
 
