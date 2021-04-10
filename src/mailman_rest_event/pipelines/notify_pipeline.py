065aca540b221baffd19ba6e0c617b544c0def77
dezhidki <dezhidki@jyu.fi>
2021-04-10 21:46:45 +0300

    Fix typo

--- src/mailman_rest_event/pipelines/notify_pipeline.py
+++ src/mailman_rest_event/pipelines/notify_pipeline.py
@@ -12,16 +12,16 @@ logger.info("Initing pipeline")
 @implementer(IPipeline)
 class NotifyPipeline:
     name = 'notify-pipeline'
-    description = 'A dummy pipeline that injects to_notify handler to other pipelines.'
+    description = 'A dummy pipeline that injects to-event handler to other pipelines.'
 
     def __init__(self):
         for name, pipeline in config.pipelines.items():
             logger.info(f"Pipeline: {name}")
             try:
                 handler_names = [ h.name for h in pipeline._handlers ]
-                if "to-archive" in handler_names and "to-notify" not in handler_names:
-                    logger.info(f"Added to-notify to {name}")
-                    pipeline._handlers.append(config.handlers["to-notify"])
+                if "to-archive" in handler_names and "to-event" not in handler_names:
+                    pipeline._handlers.append(config.handlers["to-event"])
+                    logger.info(f"Added to-event to {name}")
             except Exception as e:
                 logger.error(f"Error: {e}")
 
