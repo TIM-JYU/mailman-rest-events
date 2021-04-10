a4afbbcc9c9021a83244511cace802498cf72268
dezhidki <dezhidki@jyu.fi>
2021-04-10 10:50:40 +0300

    Add initial event handler

--- src/mailman_rest_event/plugin.py
+++ src/mailman_rest_event/plugin.py
@@ -3,10 +3,17 @@ import logging
 from mailman.interfaces.plugin import IPlugin
 from public import public
 from zope.interface import implementer
+from zope.event import subscribers
 
 logger = logging.getLogger("mailman.plugins")
 
 logger.info("RestEventPlugin start!")
+
+
+def handle_event(evt):
+    logger.info(f"Got event: {type(evt)}")
+
+subscribers.append(evt)
 
 @public
 @implementer(IPlugin)
