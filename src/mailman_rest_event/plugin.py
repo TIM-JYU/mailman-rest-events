d33ecd49027f74324ecec5a569521fa3b4eb99e2
dezhidki <dezhidki@jyu.fi>
2021-04-10 21:01:28 +0300

    Add initial handler and pipeline folders

--- src/mailman_rest_event/plugin.py
+++ src/mailman_rest_event/plugin.py
@@ -1,19 +1,35 @@
 import logging
-
+import requests
+from mailman.config import config
+from mailman.config.config import external_configuration
 from mailman.interfaces.plugin import IPlugin
 from public import public
 from zope.interface import implementer
 from zope.event import subscribers
 
+
 logger = logging.getLogger("mailman.plugins")
+logger.info(f"Pipelines: {config.pipelines}")
 
-logger.info("RestEventPlugin start!")
+def init():
+    config = external_configuration(
+        config.plugin.mailman_rest_event.configuration)
+    event_webhook_url = config.get("general", "webhook_url")
+
+    if not event_webhook_url:
+        logger.info("Webhook URL not set, will not be sending events")
+        return
+
+    logger.info(f"Webhook URL: {event_webhook_url}")
+
+    def handle_event(evt):
+        logger.info(f"Got event: {type(evt)}")
+
+    subscribers.append(handle_event)
 
 
-def handle_event(evt):
-    logger.info(f"Got event: {type(evt)}")
+init()
 
-subscribers.append(handle_event)
 
 @public
 @implementer(IPlugin)
