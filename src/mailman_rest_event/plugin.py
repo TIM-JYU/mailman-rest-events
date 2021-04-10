4252e6860da3b3ec99b38e82e183fa30632311f1
dezhidki <dezhidki@jyu.fi>
2021-04-10 22:53:30 +0300

    Add initial webhook posting

--- src/mailman_rest_event/plugin.py
+++ src/mailman_rest_event/plugin.py
@@ -7,8 +7,29 @@ from public import public
 from zope.interface import implementer
 from zope.event import subscribers
 
+from mailman.interfaces.member import SubscriptionEvent
+
 
 logger = logging.getLogger("mailman.plugins")
+
+
+def mlist_to_json(mlist):
+    return {
+        "id": mlist.list_id,
+        "name": mlist.list_name,
+        "host": mlist.mail_host,
+    }
+
+
+def member_to_json(member):
+    return {
+        "id": member.member_id,
+        "user_id": member.user_id,
+        "address": {
+            "email": member.address.email,
+            "name": member.address.display_name,
+        },
+    }
 
 
 def init():
@@ -22,8 +43,23 @@ def init():
 
     logger.info(f"Webhook URL: {event_webhook_url}")
 
+    handlers = {
+        SubscriptionEvent: (lambda evt: {
+            "event": "user_subscribed",
+            "mlist": mlist_to_json(evt.mlist),
+            "member": member_to_json(evt.member)
+        })
+    }
+
     def handle_event(evt):
-        logger.info(f"Got event: {type(evt)}")
+        t = type(evt)
+        if t in handlers:
+            try:
+                logger.info(f"Posting: {type(evt)}")
+                result = requests.post(event_webhook_url, json=handlers[t](evt))
+                logger.info(f"Result: {result.status_code}")
+            except Exception as e:
+                logger.error(f"Failed to post: {e}")
 
     subscribers.append(handle_event)
 
